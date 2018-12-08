f58122bb284667192d8280d1f5fdb2de5813ffdd
blankUsername <e@ma.il>
2018-12-08 16:30:44 +0000

    small fix

--- 1.8/web/init.sh
+++ 1.8/web/init.sh
@@ -1,4 +1,4 @@
-ln -sf . /home/box/web
+ln -sf /home/box/stepWebtech/1.8/web /home/box/web
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
