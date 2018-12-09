6ae24104ca1e5fcd3130f7a2c11fc0675959a750
Alex <email@local>
2018-12-09 12:02:02 +0300

    env

--- 1.9/web/hello.py
+++ 1.9/web/hello.py
@@ -1,7 +1,10 @@
 def app(environ, start_response):
-   data = "Hello, World!\n"
+   #data = b"Hello, World!\n"
    start_response("200 OK", [
-       ("Content-Type", "text/plain"),
-       ("Content-Length", str(len(data)))
+       ("Content-Type", "text/plain")#,
+       #("Content-Length", str(len(data))
+       )
    ])
-   return iter([data])
+   #return iter([data])
+   return [("%s: %s\n" % (key, value)).encode("utf-8")
+           for key, value in environ.items()]
