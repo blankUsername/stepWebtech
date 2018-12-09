f02d055d7df66005c3bbf27acad49105abdf3934
user <e@ma.il>
2018-12-09 10:04:04 +0000

    paras

--- 1.9/web/hello.py
+++ 1.9/web/hello.py
@@ -3,8 +3,6 @@ def app(environ, start_response):
    start_response("200 OK", [
        ("Content-Type", "text/plain")#,
        #("Content-Length", str(len(data))
-       )
    ])
    #return iter([data])
-   return [("%s: %s\n" % (key, value)).encode("utf-8")
-           for key, value in environ.items()]
+   return [ "\n".join(environ['QUERY_STRING'].split('&'))]
