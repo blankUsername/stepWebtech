187532a50af5cc2052c93434efd044135337c92a
Alex <email@local>
2018-12-09 14:39:13 +0300

    fix

--- 1.9/web/hello.py
+++ 1.9/web/hello.py
@@ -1,7 +1,8 @@
 def app(environ, start_response):
    d = "\n".join(environ['QUERY_STRING'].split('&'))
+   d=d.encode('utf-8')
    start_response("200 OK", [
        ("Content-Type", "text/plain")#,
-       ("Content-Length", str(len(data))
+       #("Content-Length", str(len(d)))
    ])
-   return iter([data.encode('utf-8')])
+   return iter([d])
